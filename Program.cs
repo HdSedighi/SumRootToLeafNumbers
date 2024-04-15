6e397f37d1a127be1a14821e5d030e805e8bd1a1
Hoda Sedighi <sedighi.iut@gmail.com>
2024-04-15 19:52:00 -0400

    call and test the solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         //call solution class
+        Solution solution = new Solution();
+        //[1,5,1,null,null,null,6]
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.right.right = new TreeNode(6);
+        
+        Console.WriteLine(solution.SumNumbers(root));
+    }
+}
